69b658f6834434c16f00621664dade1f50d39c38
knarf7112 <knarf7112@gmail.com>
2026-01-13 00:00:46 +0800

    feat: add nuxt pinia package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,26 @@ export default defineNuxtConfig({
   plugins: ['~/plugins/test.ts'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   app: {
     layoutTransition: { name: 'layout', mode: 'out-in' }
   },
+
   routeRules: {
     '/hello': { swr: 10 }
   },
+
   devServer: {
     port: 3333,
   },
+
   nitro: {
     experimental: {
       websocket: true,
       tasks: true
     },
     scheduledTasks: { "*/30 * * * * *": ['log'] }
-  }
+  },
+
+  modules: ['@pinia/nuxt']
 })
